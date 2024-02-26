b2053de021a1a13c5a481908848817bc5261a15e
Jason Krell <1910009+krelljd@users.noreply.github.com>
2024-02-26 12:43:50 -0500

    feature updates for admin section

--- Hubs/ScoreHub.cs
+++ Hubs/ScoreHub.cs
@@ -9,8 +9,8 @@ public class ScoreHub : Hub
         await Clients.All.SendAsync("UpdateScores", scores);
     }
 
-    public async Task SendTeamsMessage(string t1, string t1Color, string t2, string t2Color)
+    public async Task SendMatchInfoMessage(string t1, string t1Color, string t2, string t2Color, string tName, string sbColor)
     {
-        await Clients.All.SendAsync("UpdateTeams", t1, t1Color, t2, t2Color);
+        await Clients.All.SendAsync("UpdateMatchInfo", t1, t1Color, t2, t2Color, tName, sbColor);
     }
 }
