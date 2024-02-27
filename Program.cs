4eb70bb4b51e2971aae27fb823f4a2cd91236be0
Jason Krell <1910009+krelljd@users.noreply.github.com>
2024-02-27 08:25:36 -0500

    rpi changes. not working

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// builder.Services.AddSignalRCore();
 builder.Services.AddResponseCompression(opts => 
 {
     opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
@@ -16,9 +17,34 @@ builder.Services.AddResponseCompression(opts =>
     );
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowedOrigins",
+                      policy =>
+                      {
+                          policy.WithOrigins("http://skorbord.app", "https://skorbord.app", "http://localhost", "https://localhost");
+                          policy.WithMethods("GET", "POST");
+                          policy.AllowCredentials();
+                      });
+});
+
 builder.Services.AddFluentUIComponents();
 
 var app = builder.Build();
+
+app.UseResponseCompression();
+
+var webSocketOptions = new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromMinutes(2)
+};
+
+webSocketOptions.AllowedOrigins.Add("https://skorbord.app");
+webSocketOptions.AllowedOrigins.Add("http://skorbord.app");
+webSocketOptions.AllowedOrigins.Add("https://localhost");
+webSocketOptions.AllowedOrigins.Add("http://localhost");
+
+app.UseWebSockets(webSocketOptions);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -26,17 +52,15 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
-app.UseResponseCompression();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+    
 app.MapHub<ScoreHub>("/scorehub");
-
 
 app.Run();
